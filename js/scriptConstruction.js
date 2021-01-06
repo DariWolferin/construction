8b9f04918345cbcc9fd603dc70dad34ee7b5dbde
DariWolferin <65591301+DariWolferin@users.noreply.github.com>
2021-01-06 22:20:05 +0300

    Add files via upload

---
+++ js/scriptConstruction.js
@@ -0,0 +1,18 @@
+new Swiper('.main-slider', {
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+	},
+
+	grabCursor: true,
+
+	autoplay: true,
+
+	effect: 'fade',
+	fadeEffect: {
+	   crossFade: true
+	},
+
+	loop: true,
+
+});
